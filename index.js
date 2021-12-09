bb184c202f88d538d53a2d45667551677077a8b3
rongwei <rongwei_alisa@hotmail.com>
2021-12-09 19:07:50 +0800

    feat: 支持wp5

--- index.js
+++ index.js
@@ -29,54 +29,166 @@ class MKThemePlugin {
 
   apply(compiler) {
     const options = this.options;
-    compiler.plugin("emit", function (compilation, callback) {
-      const less = `
-    <link rel="stylesheet/less" type="text/css" href="${options.publicPath}/color.less" />
-    <script>
-      window.less = {
-        async: ${options.async},
-        env: 'production',
-        javascriptEnabled: true
-      };
-    </script>
-    <script type="text/javascript" src="${options.lessUrl}"></script>
-        `;
-      if (
-        options.indexFileName &&
-        options.indexFileName in compilation.assets
-      ) {
-        const index = compilation.assets[options.indexFileName];
-        let content = index.source();
-
-        if (!content.match(/\/color\.less/g)) {
-          index.source = () =>
-            content.replace(less, "").replace(/<body(.*?)>/gi, `<body$1>${less}`);
-          content = index.source();
-          index.size = () => content.length;
-        }
-      }
-      if (options.generateOnce && this.colors) {
-        compilation.assets["color.less"] = {
-          source: () => this.colors,
-          size: () => this.colors.length
+    const { webpack } = compiler;
+    const { sources, Compilation } = webpack;
+    const { RawSource } = webpack.sources;
+    const pluginName = 'MKThemePlugin';
+    let hasColorLess = false;
+    compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
+      compilation.hooks.processAssets.tap({
+        name: pluginName,
+        stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+      }, (assets) => {
+        const less = `
+      <link rel="stylesheet/less" type="text/css" href="${options.publicPath}/color.less" />
+      <script>
+        window.less = {
+          async: ${options.async},
+          env: 'production',
+          javascriptEnabled: true
         };
-        return callback();
-      }
-      generateTheme(options)
-        .then(css => {
-          if (options.generateOnce) {
-            this.colors = css;
-          }
-          compilation.assets["color.less"] = {
-            source: () => css,
-            size: () => css.length
-          };
-          callback();
-        })
-        .catch(err => {
-          callback(err);
-        });
+      </script>
+      <script type="text/javascript" src="${options.lessUrl}"></script>
+    `;
+        if (
+          options.indexFileName &&
+          options.indexFileName in compilation.assets
+        ) {
+          const file = compilation.getAsset(options.indexFileName);
+          compilation.updateAsset(
+            options.indexFileName,
+            new sources.RawSource(file.source.source().replace(less, "").replace(/<body(.*?)>/gi, `<body$1>${less}`))
+          );
+        }
+      })
+
+      compilation.hooks.processAssets.tapAsync({
+        name: `${pluginName}-generateless`,
+        stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+      }, (assets, callback) => {
+        if (hasColorLess) {
+          if (typeof callback === 'function') callback();
+        } else {
+          generateTheme(options)
+            .then(css => {
+              if (options.generateOnce) {
+                this.colors = css;
+              }
+              hasColorLess = true;
+              compilation.emitAsset(
+                'color.less',
+                new RawSource(css),
+              )
+              if (typeof callback === 'function') callback();
+            })
+            .catch(err => {
+              if (typeof callback === 'function') callback(err);
+            });
+        }
+
+      })
     });
+    // compiler.hooks.emit.tap(pluginName, (compilation) => {           
+    // const less = `
+    //   <link rel="stylesheet/less" type="text/css" href="${options.publicPath}/color.less" />
+    //   <script>
+    //     window.less = {
+    //       async: ${options.async},
+    //       env: 'production',
+    //       javascriptEnabled: true
+    //     };
+    //   </script>
+    //   <script type="text/javascript" src="${options.lessUrl}"></script>
+    // `;
+    // if (
+    //   options.indexFileName &&
+    //   options.indexFileName in compilation.assets
+    // ) {
+    //   const file = compilation.getAsset(options.indexFileName);
+    //   compilation.updateAsset(
+    //     options.indexFileName,
+    //     new sources.RawSource(file.source.source().replace(less, "").replace(/<body(.*?)>/gi, `<body$1>${less}`))
+    //   );
+    // }
+    // })
+
+    // compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
+    //   // Tapping to the assets processing pipeline on a specific stage.
+    //   compilation.hooks.processAssets.tapAsync({
+    //     name: pluginName,
+    //     stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+    //   }, (assets, callback) => {
+    //     if (hasColorLess) {
+    //       if (typeof callback === 'function') callback();
+    //     } else {
+    //       generateTheme(options)
+    //         .then(css => {
+    //           if (options.generateOnce) {
+    //             this.colors = css;
+    //           }
+    //           hasColorLess = true;
+    //           compilation.emitAsset(
+    //             'color.less',
+    //             new RawSource(css),
+    //           )
+    //           if (typeof callback === 'function') callback();
+    //         })
+    //         .catch(err => {
+    //           if (typeof callback === 'function') callback(err);
+    //         });
+    //     }
+
+    //   })
+    // })
+
+    // compiler.plugin("emit", function (compilation, callback) {
+    //   const less = `
+    // <link rel="stylesheet/less" type="text/css" href="${options.publicPath}/color.less" />
+    // <script>
+    //   window.less = {
+    //     async: ${options.async},
+    //     env: 'production',
+    //     javascriptEnabled: true
+    //   };
+    // </script>
+    // <script type="text/javascript" src="${options.lessUrl}"></script>
+    //     `;
+    //   if (
+    //     options.indexFileName &&
+    //     options.indexFileName in compilation.assets
+    //   ) {
+    //     const index = compilation.assets[options.indexFileName];
+    //     let content = index.source();
+
+    //     if (!content.match(/\/color\.less/g)) {
+    //       index.source = () =>
+    //         content.replace(less, "").replace(/<body(.*?)>/gi, `<body$1>${less}`);
+    //       content = index.source();
+    //       index.size = () => content.length;
+    //     }
+    //   }
+    //   if (options.generateOnce && this.colors) {
+    //     compilation.assets["color.less"] = {
+    //       source: () => this.colors,
+    //       size: () => this.colors.length
+    //     };
+    //     return callback();
+    //   }
+    //   generateTheme(options)
+    //     .then(css => {
+    //       if (options.generateOnce) {
+    //         this.colors = css;
+    //       }
+    //       compilation.assets["color.less"] = {
+    //         source: () => css,
+    //         size: () => css.length
+    //       };
+    //       callback();
+    //     })
+    //     .catch(err => {
+    //       callback(err);
+    //     });
+    // });
 
     compiler.hooks && compiler.hooks.done.tapPromise(
       this.constructor.name,
